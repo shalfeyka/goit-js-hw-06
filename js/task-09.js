654f574b1e1d0179c9a5dd1f49a46bda0750e5bc
Marina Checheneva <m.checheneva140201@gmail.com>
2022-06-24 16:03:40 +0300

    task 9 50/50

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  bgColor: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+}
+
+refs.button.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const hexColor = getRandomHexColor();
+  refs.body.style.backgroundColor = hexColor;
+  refs.bgColor.textContent = hexColor;
+
+}
